4dfba690fa6eb454df13c51e00921bd74bb6fef2
rakesh207qa <rakeshmasapathri1@gmail.com>
2022-12-16 10:38:25 +0530

    first commit

---
+++ practice/Practice1/EncapsulationExample.java
@@ -0,0 +1,17 @@
+package com.automation.pratice;
+
+public class EncapsulationExample {
+	private int a=100;
+	int b =0;
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+	
+	
+
+}
---
+++ practice/Practice1/MyFirstClass.java
@@ -0,0 +1,13 @@
+package com.automation.pratice;
+
+public class MyFirstClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hellow world3!!");
+		System.out.println("Hellow world3!!");
+		System.out.println("Hellow world3!!");
+		System.out.println("");
+	}
+	
+}
+
